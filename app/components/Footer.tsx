0ed21efc5589ffcf16a49009f12ac6cc3dda123d
DidasMba <daniel.mbarushi@gmail.com>
2024-06-19 20:26:44 +0300

    footer fist div in process

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,25 +2,35 @@ import Link from "next/link";
 
 const Footer = ()=>{
     return(
-        <footer className="px-6 py-8 bg-customBlue text-white">
-            <div>         
-              <img
-      src="./Soracertlogo.png"
-      width={128}
-      height={82}
-      alt="soracert"
-      className="brand-2"
-      style={{ maxWidth: '100%', height: 'auto' }}
-    />
+        <footer className="px-6 py-14 bg-customBlue text-white">
+          <div className="container mx-auto py-14 px-6">
+            <div className="grid md:grid-cols-12 grid-cols-1 gap-7">
 
-             
-              <p className="">Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut 
-                labore et dolore magna aliqua.  </p> 
+                <div className="lg:col-span-4 col-span-12">        
+
+                    <a href="/">
+                    <img
+                        src="./Soracertlogo.png"
+                        width={128}
+                        height={82}
+                        alt="soracert"
+                        className="brand-2"
+                        style={{ maxWidth: '100%', height: 'auto' }}
+                    />  
+                    </a> 
+                      
+                    <p className="text-gray-300 text-base">
+                      Lorem ipsum dolor sit amet, 
+                      consectetur adipiscing elit, 
+                      sed do eiusmod tempor incididunt ut 
+                      labore et dolore magna aliqua. </p> 
+
+                  </div>
 
             </div>
-
+           </div>
+            
+          
         </footer>
       
     )
