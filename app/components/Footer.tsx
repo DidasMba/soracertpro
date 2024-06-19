763c68a9a640a912b656fd5cacf3d28e128509ac
DidasMba <daniel.mbarushi@gmail.com>
2024-06-19 15:53:14 +0300

    RootLayout on function

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 const Footer = ()=>{
     return(
-        <h1>Footer</h1>
+        <footer>
+            <div>
+                <Link href={"/"}>2023</Link>
+                <div>
+                    <p>bien</p>
+                    <p>bien</p>
+                    <p>bien</p>
+                </div>
+
+            </div>
+
+        </footer>
+      
     )
 }
 
-export default Footer;
+export default Footer; 
