be068d5d7552522567f3ce50f40d00ea1decc9b8
DidasMba <daniel.mbarushi@gmail.com>
2024-06-19 19:47:00 +0300

    logo on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,14 +2,22 @@ import Link from "next/link";
 
 const Footer = ()=>{
     return(
-        <footer className="px-6 py-2 bg-customBlue text-white">
-            <div>
-                <Link href={"/"}>2023</Link>
-                <div>
-                    <p>bien</p>
-                    <p>bien</p>
-                    <p>bien</p>
-                </div>
+        <footer className="px-6 py-8 bg-customBlue text-white">
+            <div>         
+              <img
+      src="./Soracertlogo.png"
+      width={128}
+      height={82}
+      alt="soracert"
+      className="brand-2"
+      style={{ maxWidth: '100%', height: 'auto' }}
+    />
+
+             
+              <p className="">Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua.  </p> 
 
             </div>
 
