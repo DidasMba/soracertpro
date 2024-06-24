7f6fe64d1982abf7908464d27e9b1d2a374d2be0
DidasMba <daniel.mbarushi@gmail.com>
2024-06-24 16:29:28 +0300

    tryng footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin} from "react-icons/fa";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -12,184 +14,195 @@ const Footer = ()=>{
       setYear(new Date().getFullYear());
   }, []);
     return(
-        <footer className="px-8 bg-customBlue text-white">
-           <div className="container mx-auto py-14 px-6">
-              <div className="grid md:grid-cols-12 grid-cols-1 gap-7">
+      <footer className="px-8 bg-customBlue text-white">
+      <div className="container mx-auto py-14 px-6">
+          <div className="grid md:grid-cols-12 grid-cols-1 gap-6">
+              {/* First Column */}
+              <div className="lg:col-span-4 col-span-12">
+                  <a href="/">
+                      <img
+                          src="./Soracertlogo.png"
+                          width={128}
+                          height={82}
+                          alt="soracert"
+                          className="brand-2"
+                          style={{ maxWidth: '100%', height: 'auto' }}
+                      />
+                  </a>
+                  <p className="text-gray-300 text-base">
+                      Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.
+                  </p>
+              </div>
 
-                        <div className="lg:col-span-4 col-span-12">        
+              {/* Second, Third, and Fourth Columns */}
+              <div className="lg:col-span-8 md:col-span-12 col-span-12 grid grid-cols-1 md:grid-cols-3 gap-6 mt-8">
+                  <div>
+                      <h3 className="tracking-wide text-gray-300 font-semibold">COMPANY1</h3>
+                      <ul className="list-none mt-6 space-y-2">
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  About us
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Legal information
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Contact us
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Blog
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
 
-                            <a href="/">
-                            <img
-                                src="./Soracertlogo.png"
-                                width={128}
-                                height={82}
-                                alt="soracert"
-                                className="brand-2"
-                                style={{ maxWidth: '100%', height: 'auto' }}
-                            />  
-                            </a> 
-                            
-                            <p className="text-gray-300 text-base">
-                            Lorem ipsum dolor sit amet, 
-                            consectetur adipiscing elit, 
-                            sed do eiusmod tempor incididunt ut 
-                            labore et dolore magna aliqua. </p> 
+                  <div>
+                      <h3 className="tracking-wide text-gray-300 font-semibold">COMPANY2</h3>
+                      <ul className="list-none mt-6 space-y-2">
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  About us
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Legal information
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Contact us
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Blog
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
 
-                        </div>
-                        <div className="lg:col-span-2 md:col-span-4 col-span-12">
-                           <h3 className="tracking-wide text-gray-300 font-semibold">COMPANY</h3>
-                           <ul className="list-none mt-6 space-y-2">
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  About us</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Legal information</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Contact us</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Blog</a>
-                            </li>
-
-                           </ul>
-                        </div>
-                        <div className="lg:col-span-3 md:col-span-4 col-span-12">
-                        <h3 className="tracking-wide text-gray-300 font-semibold">COMPANY</h3>
-                           <ul className="list-none mt-6 space-y-2">
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  About us</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Legal information</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Contact us</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Blog</a>
-                            </li>
-
-                           </ul>
-
-                        </div>
-                        <div className="lg:col-span-3 md:col-span-4 col-span-12 ">
-                        <h3 className="tracking-wide text-gray-300 font-semibold">COMPANY</h3>
-                           <ul className="list-none mt-6 space-y-2">
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Phone: 1234567890</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Email: company@email.com</a>
-                            </li>
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Location: 100 Smart Street, LA, USA</a>
-                            </li>
-                            
-
-                           </ul>
-                           <div className="flex space-x-4 mt-5">
-                            <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
+                  <div>
+                      <h3 className="tracking-wide text-gray-300 font-semibold">COMPANY3</h3>
+                      <ul className="list-none mt-6 space-y-2">
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Phone: 1234567890
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Email: company@email.com
+                              </a>
+                          </li>
+                          <li>
+                              <a
+                                  href="#"
+                                  className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                              >
+                                  Location: 100 Smart Street, LA, USA
+                              </a>
+                          </li>
+                      </ul>
+                      <div className="flex space-x-4 mt-5">
+                          <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
                               <FaFacebook />
-                            </a>
-                            <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
+                          </a>
+                          <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
                               <FaTwitter />
-                            </a>
-                            <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
+                          </a>
+                          <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
                               <FaInstagram />
-                            </a>
-                            <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
+                          </a>
+                          <a className="text-white hover:text-customHoverBlue transform hover:scale-150 transition-all duration-150 ease-in-out " href="">
                               <FaLinkedin />
-                            </a>
-                           </div>
+                          </a>
+                      </div>
+                  </div>
 
+                 {/* Newsletter */}
+                 <div className="mt-18">
+
+                            <div className="h-full flex justify-center mb-5 ">
+                            <div className="mr-5">
+                            <h3 className="tracking-wide text-gray-300 font-semibold">NEWSLETTER</h3>
+                                        <ul className="list-none mt-6 space-y-2">
+                                            <li>
+                                                <a
+                                                href="#"
+                                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
+                                                >
+                                                Stay upto Date</a>
+                                            </li>
+
+                                        </ul>
+
+                            </div>  
+                                <form className="w-96 relative flex" action="">
+                                <input 
+                                    className="w-full text-gray-800 p-4 h-10 rounded-full focus:outline-none border border-transparent hover:border-customHoverBlue focus:border-customHoverBlue"
+                                    type="email"
+                                    placeholder="Enter your email"
+                                    aria-label="Email address"
+                                    required 
+                                />
+                                <button className="bg-gray-500 px-8 py-2 rounded-full text-white absolute top-0 right-0" type="submit">Submit</button>
+                                </form>
+                           </div>     
                         </div>
               </div>
+
           </div>
-          
-          <div className="mt-18">
 
-            <div className="h-full flex justify-center mb-5 ">
-            <div className="mr-8">
-              <h3 className="tracking-wide text-gray-300 font-semibold">NEWSLETTER</h3>
-                           <ul className="list-none mt-6 space-y-2">
-                            <li>
-                                <a
-                                href="#"
-                                className="text-gray-300 hover:text-customHoverBlue Transition-all duration-500 ease-in-out"
-                                >
-                                  Stay upto Date</a>
-                            </li>
-
-                           </ul>
-
-            </div>
-            <form className="w-96 relative" action="" method="post">
-              <input 
-                className="w-full text-gray-800 p-4 h-10 rounded-full focus:outline-none border border-transparent hover:border-customHoverBlue focus:border-customHoverBlue sm:rounded-r-none sm:border-r-0"
-                type="email"
-                placeholder="Enter your email"
-                aria-label="Email address"
-                required 
-              />
-              <button className="bg-gray-500 px-8 py-2 mt-2 sm:mt-0 rounded-r-full text-white" type="submit">Submit</button>
-            </form>
-            </div>     
+          {/* Copyright */}
+          <div className="border-t border-slate-700 mt-4">
+              <div className="md:text-left text-center container mx-auto py-7 px-6">
+                  <p className="mb-0">
+                      &copy; {year}, All Rights Reserved.
+                  </p>
+              </div>
           </div>
-          <div className="border-t border-slate-700">
-            <div className="md:text-left text-center contenair mx-auto py-7 px-6">
-              <p className="mb-0">
-                &copy;
-                 {year}
-                , All Rights Reserved.
-              </p>
-            </div>
-          </div>       
-        </footer>
+      </div>
+  </footer>
       
     )
 }
