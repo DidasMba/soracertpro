231544f9fffc9e189b90d839039a6056675e0f07
DidasMba <daniel.mbarushi@gmail.com>
2024-06-26 17:31:47 +0300

    cards process

--- app/Programs/page.tsx
+++ app/Programs/page.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='py-24 bg-gray-200'>
+    <div className='py-24 px-8 mx-auto bg-gray-200'>
         <div className='container'>
             <h4 className='uppercase front-semibold text-center mb-1 text-red-700 text-xl'>{`vienss`}</h4>
             <h2>chooser your program</h2>
-        <div>page</div>
+           <div className='grid lg:grid-cols-3 gap-5'>
+             <div className='bg-green-500'>Fleet</div>
+             <div className='bg-green-500'>Fleet</div>
+             <div className='bg-green-500'>Fleet</div>
+           </div>
         </div>
        
     </div>
