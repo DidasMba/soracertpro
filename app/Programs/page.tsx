6be20fc0e15c047786f847e027d284e2d7e13915
DidasMba <daniel.mbarushi@gmail.com>
2024-06-28 16:48:21 +0300

    cours en ligne texte

--- app/Programs/page.tsx
+++ app/Programs/page.tsx
@@ -10,10 +10,8 @@ export default function Fleets() {
       <div className='container'>
         
           <div className="ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full ">  
-          <a href="imagetitle">Workshop</a> 
+          <a href="#">Workshop</a> 
           </div>  
-          
-
 
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
@@ -62,7 +60,7 @@ export default function Fleets() {
       <div className='container'>
         
           <div className="ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full ">  
-          <a href="imagetitle">Workshop</a>
+          <a href="#">Workshop</a>
           </div>  
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
@@ -107,13 +105,17 @@ export default function Fleets() {
           </div>
       </div>
       
+
+    
       <div className='container'>
+
+          <h1 className="px-10 py-6 ">Online Courses</h1>
         
           <div className="ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full ">  
           <a href="#" className=" px-4">Workshop</a>
           <a href="#" className=" px-4">Workshop</a>
           <a href="#" className=" px-4">Workshop</a>
-          <a href="#" className=" px-4">Workshop</a>
+         
           </div> 
 
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
