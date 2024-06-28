25699907c31143191d196a470db711e6e3cf482b
DidasMba <daniel.mbarushi@gmail.com>
2024-06-28 16:08:54 +0300

    px distance

--- app/Programs/page.tsx
+++ app/Programs/page.tsx
@@ -10,11 +10,11 @@ export default function Fleets() {
       <div className='container'>
         
           <div className="ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full ">  
-          <a href="imagetitle">Workshop</a>
-          </div>
+          <a href="imagetitle">Workshop</a> 
+          </div>  
           
-          
-        
+
+
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
                 <div className='relative image-rounded'>
@@ -58,7 +58,113 @@ export default function Fleets() {
           </div>
       </div>
       
+
+      <div className='container'>
+        
+          <div className="ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full ">  
+          <a href="imagetitle">Workshop</a>
+          </div>  
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
+            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
+                <div className='relative image-rounded'>
+                    <Image
+                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
+                </div>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset
+                    </div>
+                      <p className="text-gray-700 text-base">
+                        Lo ipsum dolor sit amet, consectetur adipisicing.
+                      </p>
+               </div>
+            </div>
+            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
+                <div className='relative image-rounded'>
+                    <Image
+                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
+                </div>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      Lo ipsum dolor sit amet, consectetur adipisicing.
+                    </p>
+               </div>
+            </div>
+            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
+                <div className='relative image-rounded'>
+                    <Image
+                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
+                </div>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      Lo ipsum dolor sit amet, consectetur adipisicing.
+                    </p>
+               </div>
+            </div>
+          </div>
+      </div>
+      
+      <div className='container'>
+        
+          <div className="ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full ">  
+          <a href="imagetitle" className=" px-4">Workshop</a>
+          <a href="imagetitle" className=" px-4">Workshop</a>
+          <a href="imagetitle" className=" px-4">Workshop</a>
+          <a href="imagetitle" className=" px-4">Workshop</a>
+          </div> 
+
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
+            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
+                <div className='relative image-rounded'>
+                    <Image
+                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
+                </div>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset
+                    </div>
+                      <p className="text-gray-700 text-base">
+                        consectetur adipisicing.
+                      </p>
+               </div>
+            </div>
+            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
+                <div className='relative image-rounded'>
+                    <Image
+                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
+                </div>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      Lo ipsum dolor sit amet, consectetur adipisicing.
+                    </p>
+               </div>
+            </div>
+            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
+                <div className='relative image-rounded'>
+                    <Image
+                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
+                </div>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      Lo ipsum dolor sit amet, consectetur adipisicing.
+                    </p>
+               </div>
+            </div>
+          </div>
+      </div>
+      
     </div>
+
+    
+
+
+
 
 
   )
