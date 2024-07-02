d7c78e355944e5705ff606cdedc1aac9c4ab4423
DidasMba <daniel.mbarushi@gmail.com>
2024-07-02 20:07:22 +0300

    past events

--- app/Events/page.tsx
+++ app/Events/page.tsx
@@ -160,15 +160,14 @@ export default function Fleets() {
 
       <div className='container'>
 
-<h1 className="px-10 py-6 font-bold text-3xl text-center ">Past Events</h1>
+     <h1 className="px-10 py-6 font-bold text-3xl text-center ">Past Events</h1>
 
 
   <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
     <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
     <div className='relative image-rounded'>
             <Image
-            className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-            
+            className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />  
            
         </div>
         <div className="px-6 py-4">  
@@ -178,43 +177,19 @@ export default function Fleets() {
     <div className='relative image-rounded'>
             <Image
             className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-            <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                29 June 2024 {/* Date à ajouter ici */}
-            </div>
-            <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-            </div>
+            
+         
         </div>
-        <div className="px-6 py-4">
-            <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-            </div>
-            <p className="text-gray-700 text-base">
-            Localisation
-            </p>
-      </div>
+      
     </div>
     <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
     <div className='relative image-rounded'>
             <Image
             className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-            <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                29 June 2024 {/* Date à ajouter ici */}
-            </div>
-            <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-            </div>
+           
+          
         </div>
-        <div className="px-6 py-4">
-            <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-            </div>
-            <p className="text-gray-700 text-base">
-            Localisation
-            </p>
-      </div>
+      
     </div>
   </div>
       </div>
