35635b87dcdaf7047049db7986623a750a7ba2d5
king <b.kingsharoon@gmail.com>
2024-07-08 21:58:14 +0200

    Git add membership and career

--- components/career/landing.tsx
+++ components/career/landing.tsx
@@ -4,8 +4,8 @@ const HeroSection = () => {
   return (
     <div className="relative overflow-hidden sm:h-screen h-auto bg-no-repeat bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: "url(/Frame6821.png)" }}>
       <div className="container p-10 mx-auto max-w-8xl flex justify-start items-center">
-        <div className="content headernow2 p-8 rounded-3xl max-w-3xl items-center">
-          <h2 className="text-white text-7xl font-bold mb-10">Find A <span className="text-[#53dffc]">Job</span> That
+        <div className="content  p-2 sm:p-8 rounded-3xl max-w-3xl items-center">
+          <h2 className="text-white  text-4xl sm:text-7xl font-bold mb-10">Find A <span className="text-[#53dffc]">Job</span> That
             <span className="text-[#53dffc]"> Matches</span> Your Passion 
           </h2>
           <p className="mt-4 text-white max-w-2xl mx-auto text-xl py-2 font-extralight">
