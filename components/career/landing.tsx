b7edd69cce10ec8cb32e51225974e7198181c16f
king <b.kingsharoon@gmail.com>
2024-07-08 22:00:58 +0200

    Git add membership and career

--- components/career/landing.tsx
+++ components/career/landing.tsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
                 <div className="flex w-full max-w-md items-center rounded-full bg-white px-4 py-2">
                   <input
                     className="flex h-10 w-full rounded-full border-none border-input px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex-1 bg-transparent text-sm outline-none"
-                    placeholder="Your Email..."
+                    placeholder="Search by job title......................"
                     type="email"
                     id="newsletter-email"
                   />
