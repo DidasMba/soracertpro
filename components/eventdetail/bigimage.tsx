fb67b77e71418bc944d6051fb9ad2543248e0b13
DidasMba <daniel.mbarushi@gmail.com>
2024-07-17 20:33:41 +0300

    rounded full

--- components/eventdetail/bigimage.tsx
+++ components/eventdetail/bigimage.tsx
@@ -4,20 +4,20 @@ import type { NextPage } from "next";
 const FrameComponent6 = () => {
   return (
        
-<section className="flex flex-col md:flex-ro justify-center items-center py-12 px-4"  data-aos="fade-up">
-<div className="m-4 grid gap-6 sm:grid-cols-10">
-     {/* Première colonne */}
-     <div className="sm:col-span-4 min-h-[270px] image-rounded  shadow ml-14 relative">
-        <img src="/latest property 1.png" alt="Pro 1" />
-     </div>
-     {/* Deuxième colonne */}
-   
-</div>
+<section className="">
+  <div className="">
+            <div className='max-w-[1564px] h-[350px]  mx-auto p-8 rounded-full'>
+                <img
+                    src="/soraimg1.JPG"
+                    alt='lg'
+                    className='w-full h-full object-cover rounded-full'
+                />
+            </div>
 
-
-
-
-      </section>
+  </div>
+            
+    
+</section>
   );
 };
 
