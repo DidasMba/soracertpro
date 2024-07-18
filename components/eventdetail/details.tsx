3d25f4a26f7a70afc6972c0e417f76b59f1d4091
DidasMba <daniel.mbarushi@gmail.com>
2024-07-18 14:25:16 +0300

    add calendar

--- components/eventdetail/details.tsx
+++ components/eventdetail/details.tsx
@@ -16,8 +16,8 @@ const HeroSection = () => {
             <p className="text-sm font-light text-gray-400 pt-4 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim justo vel sagittis pellentesque. Donec auctor quis lectus vel rhoncus. Integer vehicula vehicula sem eget egestas. Duis a leo at enim tempus interdum sit amet sit amet lacus. Morbi in risus turpis. Pellentesque sed lacus vel lectus auctor placerat. Praesent lobortis viverra orci.
 
 Praesent eu congue mi. Aenean at risus et purus elementum aliquet. Fusce tristique lacus dolor, ac facilisis sem aliquet eu. In hac habitasse platea dictumst. Cras lobortis vulputate cursus. Duis nec nisi venenatis, ultrices metus sit amet, aliquam augue. In nec ante nunc. Fusce ornare justo a velit mattis bibendum viverra sit amet erat. </p>
-            <button className=" text-white font-bold py-2 px-4 text-white  bg-customBlue">Buy</button>
-            <a className="hidden lg:inline-block py-2 px-6 text-sm text-white font-bold rounded-xl transition duration-200" href="#">Sign up</a>
+            <button className=" text-white font-bold py-6 px-4  bg-customBlue">Sign up</button>
+            <a className="hidden lg:inline-block py-2 text-sm  font-bold rounded-xl transition duration-200" href="#">Sign up</a>
           </div>
         </div>
       </div>
