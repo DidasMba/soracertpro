482adc8c4d389e6b48c62f3516f2c8ce647945a9
DidasMba <daniel.mbarushi@gmail.com>
2024-07-19 16:27:02 +0300

    store in process

---
+++ app/store/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import LandingPage from "@/components/store/landing";
+import Products from "@/components/store/products";
+
+export default function AboutUs() {
+  return (
+    <main className="">
+  
+   <LandingPage />
+    <Products />
+    
+    </main>
+  );
+}
