92db2e925b4544b00e2e00a905ed87203ecac551
DidasMba <daniel.mbarushi@gmail.com>
2024-07-23 23:38:07 +0300

    structure

--- components/eventdetail/details.tsx
+++ components/eventdetail/details.tsx
@@ -4,77 +4,76 @@ const HeroSection = () => {
     return (
      
      
-      <section className="flex flex-col md:flex-ro justify-center items-center py-12 px-4"  data-aos="fade-up">
+    <section className="flex flex-col md:flex-ro justify-center items-center py-12 px-4"  data-aos="fade-up">
       
         <div className="flex flex-col md:flex-row w-full max-w-6xl w items-center justify-between space-y-8 md:space-y-0 md:space-x-8">
-   
-      <div className="">
-        <div className="grid sm:grid-cols-2 gap-2 p-12 ">
-      
-        <div className="">
-         
-          <div className=" p-8 gap-2 px-0  bg-transparent" >
-                <div className="team-member  flex flex-row  justify-start items-center">
-                <div className="pic mr-3"><img src="/calendarm.png" style={{height:'50px', width:'50px' , objectFit:'cover'}} className="img-fluid" alt="" /></div>
-                <div className="member-info">
-                <h4 className="stretched-link text-lg font-bold">Friday</h4>
-                <p className="texl-sm font-extralight">May 24, 2024</p>
-              </div>
-              </div>
+             <div className="grid sm:grid-cols-2 gap-2 p-12 ">
+              
+                <div className="">
+                
+                      <div className=" p-8 gap-2 px-0  bg-transparent" >
+                            <div className="team-member  flex flex-row  justify-start items-center">
+                                <div className="pic mr-3"><img src="/calendarm.png" style={{height:'50px', width:'50px' , objectFit:'cover'}} className="img-fluid" alt="" /></div>
+                            <div className="member-info">
+                                <h4 className="stretched-link text-lg font-bold">Friday</h4>
+                                <p className="texl-sm font-extralight">May 24, 2024</p>
+                            </div>
+                          </div>
+                      </div>
+                    
+                    <div className="border-b border-gray-200 mx-4 w-3/5">
+                    </div>
+
+                    <div className="flex gap-4 ">
+                          <div className=" p-8 gap-2 px-0 bg-transparent" >
+                                  <div className="team-member  flex flex-row  justify-start items-center">
+                                      <div className="pic mr-3"><img src="/clocks.png" style={{height:'50px', width:'50px' , objectFit:'cover'}} className="img-fluid" alt="" /></div>
+                                      <div className="member-info">
+                                      <h4 className="stretched-link text-lg font-bold">Friday</h4>
+                                      <p className="texl-sm font-extralight">May 24, 2024</p>
+                                      </div>
+                                  </div>
+                          </div>
+
+                          <div className=" p-8 gap-2 px-3 bg-transparent" >
+                                  <div className="team-member  flex flex-row  justify-start items-center">
+                                
+                                  <div className="member-info">
+                                  <h4 className="stretched-link text-lg font-bold">Friday</h4>
+                                  <p className="texl-sm font-extralight">May 24, 2024</p>
+                                </div>
+                                </div>
+                          </div>
+                    
+
+                     </div>
+                
+                    
+                
+                    
+                    <button className="w-[157px] py-2 rounded-4xl bg-customBlue flex flex-row items-start justify-start pt-3 px-[19px] pb-[11px] box-border whitespace-nowrap z-[1] text-center text-sm text-white" style={{ borderRadius: '20px' }}>
+                            <a className="[text-decoration:none] h-[23px] flex-1 relative font-semibold text-[inherit] inline-block z-[1]">
+                              Add Calendar
+                            </a>
+                    </button>
+
+                    
+                  </div>
+                  
+                  <div>
+                    <h1 className="font-bold text-lg">About thin Event</h1>
+                    <p className="text-sm font-light  pt-4 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim justo vel sagittis pellentesque. Donec auctor quis lectus vel rhoncus. Integer vehicula vehicula sem eget egestas. Duis a leo at enim tempus interdum sit amet sit amet lacus. Morbi in risus turpis. Pellentesque sed lacus vel lectus auctor placerat. Praesent lobortis viverra orci.
+
+                      Praesent eu congue mi. Aenean at risus et purus elementum aliquet. Fusce tristique lacus dolor, ac facilisis sem aliquet eu. In hac habitasse platea dictumst. Cras lobortis vulputate cursus. Duis nec nisi venenatis, ultrices metus sit amet, aliquam augue. In nec ante nunc. Fusce ornare justo a velit mattis bibendum viverra sit amet erat. </p>
+                  <h1 className="  text-lg">I want to buy</h1>
+                    
+                    
+                  </div>
            </div>
-         
-         <div className="border-b border-gray-200 mx-4 w-3/5"></div>
-
-         <div className="flex gap-4 ">
-         <div className=" p-8 gap-2 px-0 bg-transparent" >
-                <div className="team-member  flex flex-row  justify-start items-center">
-                <div className="pic mr-3"><img src="/clocks.png" style={{height:'50px', width:'50px' , objectFit:'cover'}} className="img-fluid" alt="" /></div>
-                <div className="member-info">
-                <h4 className="stretched-link text-lg font-bold">Friday</h4>
-                <p className="texl-sm font-extralight">May 24, 2024</p>
-              </div>
-              </div>
-         </div>
-
-         <div className=" p-8 gap-2 px-3 bg-transparent" >
-                <div className="team-member  flex flex-row  justify-start items-center">
-               
-                <div className="member-info">
-                <h4 className="stretched-link text-lg font-bold">Friday</h4>
-                <p className="texl-sm font-extralight">May 24, 2024</p>
-              </div>
-              </div>
-         </div>
             
 
-         </div>
-         
-            
-        
-            
-            <button className="w-[157px] py-2 rounded-4xl bg-customBlue flex flex-row items-start justify-start pt-3 px-[19px] pb-[11px] box-border whitespace-nowrap z-[1] text-center text-sm text-white" style={{ borderRadius: '20px' }}>
-                    <a className="[text-decoration:none] h-[23px] flex-1 relative font-semibold text-[inherit] inline-block z-[1]">
-                      Add Calendar
-                    </a>
-            </button>
 
-            
-          </div>
-          
-          <div>
-            <h1 className="font-bold text-lg">About thin Event</h1>
-            <p className="text-sm font-light  pt-4 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim justo vel sagittis pellentesque. Donec auctor quis lectus vel rhoncus. Integer vehicula vehicula sem eget egestas. Duis a leo at enim tempus interdum sit amet sit amet lacus. Morbi in risus turpis. Pellentesque sed lacus vel lectus auctor placerat. Praesent lobortis viverra orci.
-
-              Praesent eu congue mi. Aenean at risus et purus elementum aliquet. Fusce tristique lacus dolor, ac facilisis sem aliquet eu. In hac habitasse platea dictumst. Cras lobortis vulputate cursus. Duis nec nisi venenatis, ultrices metus sit amet, aliquam augue. In nec ante nunc. Fusce ornare justo a velit mattis bibendum viverra sit amet erat. </p>
-           <h1 className="  text-lg">I want to buy</h1>
-            
-            
-          </div>
         </div>
-      </div>
-
-
-      </div>
       </section>
     
 
