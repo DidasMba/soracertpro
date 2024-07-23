52d91fe973bda5a2074e865b194645587a4c3e66
DidasMba <daniel.mbarushi@gmail.com>
2024-07-23 23:18:27 +0300

    change

--- app/Programs/page.tsx
+++ app/Programs/page.tsx
@@ -220,7 +220,7 @@ export default function Fleets() {
 
           <h1 className="px-10 py-6 font-bold text-3xl ">Online Courses</h1>
         
-          <div className="ok ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full workshop-list flex flex-col sm:flex-row sm:justify-between ">  
+          <div className="ok ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full workshop-list ">  
           <a href="#" className=" px-4">Workshop</a>
           <a href="#" className=" px-4">Online Courses</a>
           <a href="#" className=" px-4">Mentoring</a>
