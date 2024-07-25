5fc11e683c765b283492a56e7c93ed9f9a4dfe26
DidasMba <daniel.mbarushi@gmail.com>
2024-07-25 15:29:43 +0300

    past event

--- components/eventsev/mainevents.tsx
+++ components/eventsev/mainevents.tsx
@@ -12,16 +12,16 @@ const FrameComponent6 = () => {
           </div>  
 
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
-          <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-            <div className="h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover w-full h-full"
-                     src="/ss3.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
+            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
+               <div className='relative image-rounded'>
+                  <div className="h-80"> {/* Dimensions fixes w-80 h-80*/}
+                        <Image
+                           className="imagepro object-cover w-full h-full"
+                           src="/ss3.jpg" 
+                           alt="Pro 1"
+                           width={500}
+                           height={500} 
+                           />
                   </div>
           
 
@@ -35,25 +35,25 @@ const FrameComponent6 = () => {
                           <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
                           <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
                      </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">The Coldest Sunset
-                    </div>
-                    <p className="text-gray-700 text-base">
-                      Lo ipsum dolor sit amet, consectetur adipisicing.
-                    </p>
-               </div>
+                  </div>
+                  <div className="px-6 py-4">
+                     <div className="texteone font-bold text-xl mb-2">The Coldest Sunset
+                     </div>
+                     <p className="text-gray-700 text-base">
+                        Lo ipsum dolor sit amet, consectetur adipisicing.
+                     </p>
+                  </div>
             </div>
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
             <div className='relative image-rounded'>
-            <div className=" h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover h-full"
-                     src="/ss1.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
+                 <div className="h-80"> {/* Dimensions fixes w-80 h-80*/}
+                        <Image
+                           className="imagepro object-cover w-full h-full"
+                           src="/ss3.jpg" 
+                           alt="Pro 1"
+                           width={500}
+                           height={500} 
+                           />
                   </div>
 
 
@@ -77,14 +77,14 @@ const FrameComponent6 = () => {
             </div>
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
             <div className='relative image-rounded'>
-            <div className=" h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover h-full"
-                     src="/ss2.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
+                 <div className="h-80"> {/* Dimensions fixes w-80 h-80*/}
+                        <Image
+                           className="imagepro object-cover w-full h-full"
+                           src="/ss3.jpg" 
+                           alt="Pro 1"
+                           width={500}
+                           height={500} 
+                           />
                   </div>
 
 
@@ -105,7 +105,7 @@ const FrameComponent6 = () => {
                       Lo ipsum dolor sit amet, consectetur adipisicing.
                     </p>
                </div>
-            </div>
+              </div>
           </div>
       </div>
       
@@ -119,14 +119,14 @@ const FrameComponent6 = () => {
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
             <div className='relative image-rounded'>
                     
-            <div className="h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover w-full h-full"
-                     src="/ss4.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
+                  <div className="h-80"> {/* Dimensions fixes w-80 h-80*/}
+                        <Image
+                           className="imagepro object-cover w-full h-full"
+                           src="/ss3.jpg" 
+                           alt="Pro 1"
+                           width={500}
+                           height={500} 
+                           />
                   </div>
 
 
@@ -150,14 +150,14 @@ const FrameComponent6 = () => {
             </div>
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
             <div className='relative image-rounded'>
-            <div className="h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover w-full h-full"
-                     src="/ss4.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
+                  <div className="h-80"> {/* Dimensions fixes w-80 h-80*/}
+                        <Image
+                           className="imagepro object-cover w-full h-full"
+                           src="/ss3.jpg" 
+                           alt="Pro 1"
+                           width={500}
+                           height={500} 
+                           />
                   </div>
 
 
@@ -180,117 +180,14 @@ const FrameComponent6 = () => {
             </div>
             <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
             <div className='relative image-rounded'>
-            <div className="h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover w-full h-full"
-                     src="/ss4.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
-                  </div>
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">The Coldest Sunset
-                    </div>
-                    <p className="text-gray-700 text-base">
-                      Lo ipsum dolor sit amet, consectetur adipisicing.
-                    </p>
-               </div>
-            </div>
-          </div>
-      </div>
-      
-
-    
-      <div className='container'>
-
-          <h1 className="px-10 py-6 font-bold text-3xl ">Online Courses</h1>
-        
-          <div className="ok ok relative bg-customBlue h-10 px-6 py-2 text-white rounded-full workshop-list ">  
-          <a href="#" className=" px-4">Workshop</a>
-          <a href="#" className=" px-4">Online Courses</a>
-          <a href="#" className=" px-4">Mentoring</a>
-          <a href="#" className=" px-4">Workshops</a>
-         
-          </div> 
-
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-                    
-            <div className=" h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover h-full"
-                     src="/ss2.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
-                  </div>
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">The Coldest Sunset
-                    </div>
-                      <p className=" text-gray-700 text-base">
-                        consectetur adipisicing.
-                      </p>
-               </div>
-            </div>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-            <div className="h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover w-full h-full"
-                     src="/ss5.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
-                  </div>
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">The Coldest Sunset
-                    </div>
-                    <p className="text-gray-700 text-base">
-                      Lo ipsum dolor sit amet, consectetur adipisicing.
-                    </p>
-               </div>
-            </div>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-            <div className="h-64"> {/* Dimensions fixes w-80 h-80*/}
-                  <Image
-                     className="imagepro object-cover w-full h-full"
-                     src="/pro1.jpg" 
-                     alt="Pro 1"
-                     width={500}
-                     height={500} 
-                     />
+                 <div className="h-80"> {/* Dimensions fixes w-80 h-80*/}
+                        <Image
+                           className="imagepro object-cover w-full h-full"
+                           src="/ss3.jpg" 
+                           alt="Pro 1"
+                           width={500}
+                           height={500} 
+                           />
                   </div>
                      <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
                         29 June 2024 {/* Date à ajouter ici */}
--- components/eventsev/pevents.tsx
+++ components/eventsev/pevents.tsx
@@ -4,9 +4,37 @@ import Image from 'next/image';
 
 const FrameComponent6 = () => {
   return (
-    <div className='py-24'>
-     
-    </div>
+   
+
+<div className='container'>
+<h1 className="px-10 py-6 font-bold text-3xl text-center ">Past Events</h1>
+
+<div className="m-4 grid gap-6 sm:grid-cols-10">
+     {/* Première colonne */}
+     <div className="sm:col-span-4 min-h-[270px] image-rounded  shadow ml-14 relative">
+        <Image    src="/ss3.jpg"  alt="Pro 1" layout="fill" objectFit="cover" />
+     </div>
+     {/* Deuxième colonne */}
+     <div className="sm:col-span-6 min-h-[270px] image-rounded shadow mr-12 relative">
+        <Image    src="/ss3.jpg"  alt="Pro 1" layout="fill" objectFit="cover" />
+     </div>
+</div>
+
+<div className="m-4 grid gap-6 sm:grid-cols-10">
+
+      {/* Deuxième colonne */}
+      <div className="sm:col-span-6 min-h-[270px] image-rounded shadow  ml-14 relative">
+        <Image    src="/ss5.jpg" alt="Pro 1" layout="fill" objectFit="cover" />
+     </div>
+     {/* Première colonne */}
+     <div className="sm:col-span-4 min-h-[270px] image-rounded  shadow mr-12 relative">
+        <Image    src="/ss5.jpg"  alt="Pro 1" layout="fill" objectFit="cover" />
+     </div>
+    
+</div>
+</div>
+ 
+   
   );
 };
 
