9d20194336b597e62479e6409513e598cd5e9e17
DidasMba <daniel.mbarushi@gmail.com>
2024-08-23 17:41:56 +0300

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,8 +14,10 @@ const Navbar: React.FC = () => {
       <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
         {/* Conteneur du logo */}
         <div className="flex items-center space-x-3 rtl:space-x-reverse">
-          <a href="#" className="flex items-center">
-          <img src="/logoblack.png" alt="soracert Logo" width={100} height={100} className="h-auto w-auto" />
+        <a href="#" className="flex items-center">
+           
+            <img src="/logoblack.png" alt="soracert Logo" width={100} height={100} className="h-8" />
+       
           </a>
         </div>
         
@@ -34,7 +36,7 @@ const Navbar: React.FC = () => {
                 Projets <FaChevronDown className="w-2.5 h-2.5 ms-3" />
               </button>
               {isDropdownOpen && (
-                <div id="mega-menu-dropdown" className="absolute z-10 grid w-auto md:w-[500px] md:top-10 md:right-0 grid-cols-2 text-sm bg-white border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
+                <div id="mega-menu-dropdown" className="absolute z-10 grid w-auto md:w-[500px] md:top-10 md:right-100 grid-cols-2 text-sm bg-white border border-gray-100 shadow-md dark:border-gray-700 md:grid-cols-3 font-normal rounded dark:bg-gray-700">
                   <div className="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
                     <ul className="space-y-4" aria-labelledby="mega-menu-dropdown-button">
                       <li>
@@ -116,8 +118,8 @@ const Navbar: React.FC = () => {
 
         {/* Boutons Login et Sign up */}
         <div className="flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse md:order-2">
-          <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">S'inscrire</a>
-          <a href="#" className="text-black bg-customHoverBlue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Se connecter</a>
+          <a href="#" className="text-black bg-gray-100  font-normal rounded  dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300  text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">S'inscrire</a>
+          <a href="#" className="text-black bg-customHoverBlue  font-normal rounded  hover:bg-customHoverBlue  focus:ring-4 focus:ring-blue-300 text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Se connecter</a>
           <button data-collapse-toggle="mega-menu" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
