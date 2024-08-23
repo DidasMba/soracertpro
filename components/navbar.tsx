5720f80b47476b8f73aacf625d9a7f5276bb64ca
DidasMba <daniel.mbarushi@gmail.com>
2024-08-23 15:47:40 +0300

    navprocess

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,31 +1,133 @@
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { FaSearch, FaChevronDown } from 'react-icons/fa';
-
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import React, { useState } from 'react';
+import { FaChevronDown } from 'react-icons/fa';
 
 const Navbar: React.FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <div className="bg-gray-800 overflow-hidden">
-      <a href="#home" className="float-left text-white text-center py-3 px-4 text-lg no-underline hover:bg-red-600">
-        Home
-      </a>
-      <a href="#news" className="float-left text-white text-center py-3 px-4 text-lg no-underline hover:bg-red-600">
-        News
-      </a>
-      <div className="relative float-left">
-        <button className="text-white text-lg py-3 px-4 bg-gray-800 focus:outline-none">
-          Dropdown
-          <i className="ml-2 fa fa-caret-down"></i>
-        </button>
-        <div className="absolute hidden bg-white min-w-[160px] shadow-md z-10">
-          <a href="#" className="block text-black py-3 px-4 hover:bg-gray-200">Link 1</a>
-          <a href="#" className="block text-black py-3 px-4 hover:bg-gray-200">Link 2</a>
-          <a href="#" className="block text-black py-3 px-4 hover:bg-gray-200">Link 3</a>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
+        {/* Conteneur du logo */}
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <a href="#" className="flex items-center">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          </a>
+        </div>
+        
+        {/* Conteneur des liens de navigation */}
+        <div className="justify-start hidden md:flex md:space-x-8 rtl:space-x-reverse flex-grow">
+          <ul className="flex space-x-4 md:space-x-8">
+            <li>
+              <a href="#" className="block py-2 px-3 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
+            </li>
+            <li className="relative">
+              <button 
+                id="mega-menu-dropdown-button" 
+                onClick={toggleDropdown} 
+                className="flex items-center justify-between w-full py-2 px-3 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Company <FaChevronDown className="w-2.5 h-2.5 ms-3" />
+              </button>
+              {isDropdownOpen && (
+                <div id="mega-menu-dropdown" className="absolute z-10 grid w-auto md:w-[500px] md:top-10 md:right-0 grid-cols-2 text-sm bg-white border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
+                  <div className="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+                    <ul className="space-y-4" aria-labelledby="mega-menu-dropdown-button">
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          About Us
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Libra
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Resources
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Pro Version
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+                    <ul className="space-y-4">
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Blog
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Newsletter
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Playground
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          License
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="p-4">
+                    <ul className="space-y-4">
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Contact Us
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Support Center
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                          Terms
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              )}
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Team</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Boutons Login et Sign up */}
+        <div className="flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse md:order-2">
+          <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
+          <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</a>
+          <button data-collapse-toggle="mega-menu" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+          </button>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
