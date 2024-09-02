3a171e3903dc1b495558aa5bc452f8c72b565b3b
DidasMba <daniel.mbarushi@gmail.com>
2024-09-02 17:27:25 +0300

    Enregistrement des modifications sur Events et Navbar

--- app/Events/page.tsx
+++ app/Events/page.tsx
@@ -1,206 +1,68 @@
-import Image from "next/image"
-import React from 'react'
+import Image from "next/image";
+import React from 'react';
 import pro1 from "./images/pro1.jpg";
-//import 'swiper/css';
-//import 'swiper/css/pagination;
-//import { Pagination } from 'swiper';
 
 export default function Fleets() {
   return (
     <div className='py-2'>
-
-      <div className='container'>
-      <div className="ok ok relative bg-customBlue h-10 px-6 py-2  rounded-full flex items-center">
-      <div className="flex flex-grow space-x-4 text-white">
-    <a href="#" className="px-4">Billetterie</a>
-    <a href="#" className="px-4">Concours</a>
-    <a href="#" className="px-4">Contenu Multimédia</a>
-    <a href="#" className="px-4">Sponsors et Partenaires
-    </a>
+      <div className='container mx-auto px-4'>
+      <div className="relative bg-customBlue h-10 px-4 py-2 rounded-full flex items-center">
+  <div className="flex flex-grow space-x-2 text-white">
+    <a href="#" className="px-2 sm:px-4">Billetterie</a>
+    <a href="#" className="px-2 sm:px-4">Concours</a>
+    <a href="#" className="px-2 sm:px-4">Contenu Multimédia</a>
+    <a href="#" className="px-2 sm:px-4">Sponsors et Partenaires</a>
   </div>
+  <a href="#" className="text-black font-bold bg-customHoverBlue rounded-2xl focus:ring-4 text-xs sm:text-sm px-2 py-1 sm:px-3 sm:py-1 focus:outline-none ml-auto">
+    Programme et Agenda
+  </a>
+</div>
 
-  <a href="#" className="text-black font-bold bg-customHoverBlue rounded-2xl focus:ring-4 text-sm px-2 py-1 md:px-3 md:py-1 focus:outline-none ml-auto">
-  Programme et Agenda
-    </a>
-          </div> 
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-                    <Image
-                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
+          {[...Array(3)].map((_, index) => (
+            <div key={index} className=''>
+              <div className='relative image-rounded'>
+                <Image
+                  className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
+                <div className="absolute bottom-0 left-0 px-2 sm:px-4 py-2 bg-black bg-opacity-50 text-white text-xs sm:text-sm">
+                  29 June 2024
                 </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-                    </div>
-                      <p className="text-gray-700 text-base">
-                        Localisation 
-                      </p>
-               </div>
-            </div>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-                    <Image
-                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
+                <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
+                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
+                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
+                  <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
                 </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-                    </div>
-                    <p className="text-gray-700 text-base">
-                    Localisation.
-                    </p>
-               </div>
+              </div>
+              <div className="px-4 sm:px-6 py-4">
+                <div className="texteone font-bold text-lg sm:text-xl mb-2">Nom de l'événement</div>
+                <p className="text-gray-700 text-base">Localisation</p>
+              </div>
             </div>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-                    <Image
-                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-                    </div>
-                    <p className="text-gray-700 text-base">
-                    Localisation.
-                    </p>
-               </div>
-            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='container mx-auto px-4'>
+        <h1 className="px-4 sm:px-10 py-6 font-bold text-2xl sm:text-3xl text-center ">Événements Passés</h1>
+        <div className="grid gap-6 sm:grid-cols-10 m-4">
+          <div className="sm:col-span-4 min-h-[270px] image-rounded shadow ml-0 sm:ml-14 relative">
+            <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
           </div>
-      </div>
-
-      <div className='container'>
-
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-                <div className='relative image-rounded'>
-                    <Image
-                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-                    </div>
-                      <p className="text-gray-700 text-base">
-                      Localisation
-                      </p>
-               </div>
-             
-            </div>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-                    <Image
-                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-                    </div>
-                    <p className="text-gray-700 text-base">
-                    Localisation
-                    </p>
-               </div>
-            </div>
-            <div className=''> {/* Div contenant la generalite le texet est l image on peut y ajouter rounded-2xl  et shadow-lg dans className */}
-            <div className='relative image-rounded'>
-                    <Image
-                     className="imagepro" src={pro1} alt="Pro 1" width={500} height={500} />
-                     <div className="absolute bottom-0 left-0 px-4 py-2 bg-black bg-opacity-50 text-white text-sm">
-                        29 June 2024 {/* Date à ajouter ici */}
-                     </div>
-                     <div className="absolute bottom-0 left-0 right-0 flex justify-end pb-4 pr-3">
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                          <span className="w-2 h-2 bg-white rounded-full mx-1"></span>
-                     </div>
-                </div>
-                <div className="px-6 py-4">
-                    <div className="texteone font-bold text-xl mb-2">Nom de l'événement
-                    </div>
-                    <p className="text-gray-700 text-base">
-                    Localisation
-                    </p>
-               </div>
-            </div>
+          <div className="sm:col-span-6 min-h-[270px] image-rounded shadow mr-0 sm:mr-12 relative">
+            <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
           </div>
+        </div>
+
+        <div className="grid gap-6 sm:grid-cols-10 m-4">
+          <div className="sm:col-span-6 min-h-[270px] image-rounded shadow ml-0 sm:ml-14 relative">
+            <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
+          </div>
+          <div className="sm:col-span-4 min-h-[270px] image-rounded shadow mr-0 sm:mr-12 relative">
+            <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
+          </div>
+        </div>
       </div>
-
-      <div className='container'>
-
-<h1 className="px-10 py-6 font-bold text-3xl text-center ">Événements Passéss</h1>
- 
-<div className='container'>
-
-<div className="m-4 grid gap-6 sm:grid-cols-10">
-     {/* Première colonne */}
-     <div className="sm:col-span-4 min-h-[270px] image-rounded  shadow ml-14 relative">
-        <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
-     </div>
-     {/* Deuxième colonne */}
-     <div className="sm:col-span-6 min-h-[270px] image-rounded shadow mr-12 relative">
-        <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
-     </div>
-</div>
-
-<div className="m-4 grid gap-6 sm:grid-cols-10">
-
-      {/* Deuxième colonne */}
-      <div className="sm:col-span-6 min-h-[270px] image-rounded shadow  ml-14 relative">
-        <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
-     </div>
-     {/* Première colonne */}
-     <div className="sm:col-span-4 min-h-[270px] image-rounded  shadow mr-12 relative">
-        <Image src={pro1} alt="Pro 1" layout="fill" objectFit="cover" />
-     </div>
-    
-</div>
-</div>
-  
-      </div>
-      
-      
     </div>
-
-    
-
-
-
-
-
-  )
+  );
 }
