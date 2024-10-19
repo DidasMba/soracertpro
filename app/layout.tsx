71e9629129fae8e3b728da3b156e7a1446beca62
gracebir <grace.devolop@gmail.com>
2024-10-19 16:59:11 +0200

    remove font poppins, remove used style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,44 @@
-import type {Metadata} from 'next'
+/** @format */
+
+import type { Metadata } from "next";
 import "./globals.css";
 
-import Footer from '@/components/Footer';
-import Navbar from '@/components/navbar';
+import Footer from "@/components/Footer";
+import Navbar from "@/components/navbar";
+import { Montserrat } from "next/font/google";
 
-import  NextTopLoader  from "nextjs-toploader"
+import NextTopLoader from "nextjs-toploader";
 export const metadata: Metadata = {
-  title: 'Soracert',
-  description: 'Learn Next.js Tailwind By Components',
-}
+    title: "Soracert",
+    description: "",
+};
 
+const montserrat = Montserrat({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700", "800"],
+});
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-       <html lang="en">
-       <body className="">
-       <NextTopLoader color="#00224d"
- initialPosition={0.08}
-
- height={3}
- crawl={true}
- showSpinner={false}
- easing="ease"
-
- shadow="0 0 10px #2299DD,0 0 5px #2299DD"/>
-         <Navbar />  
-         {children}
-          <Footer />
-
-
-       </body>
-
-       </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={montserrat.className}>
+                <NextTopLoader
+                    color='#00224d'
+                    initialPosition={0.08}
+                    height={3}
+                    crawl={true}
+                    showSpinner={false}
+                    easing='ease'
+                    shadow='0 0 10px #2299DD,0 0 5px #2299DD'
+                />
+                <Navbar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
-
-
