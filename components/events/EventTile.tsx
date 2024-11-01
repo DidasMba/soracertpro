be4d42eafdd2b4a6979118af57b04031d0a28b92
didasmba <daniel.mbarushi@gmail.com>
2024-11-01 16:10:32 +0300

    img edit

--- components/events/EventTile.tsx
+++ components/events/EventTile.tsx
@@ -31,7 +31,7 @@ const EventTile: React.FC<{
                 <div className='w-full lg:w-3/4 flex flex-col lg:flex-row items-center'>
                     <div className='flex flex-col w-full lg:flex-row justify-between items-center lg:items-start'>
                         <div className='w-full lg:w-2/3 mb-4 lg:mb-0'>
-                            <span className='bg-[#d9f6f4] text-customBlue px-2 py-1 rounded uppercase text-xs font-semibold'>
+                            <span className='bg-[#81c1bd] text-customBlue px-2 py-1 rounded uppercase text-xs font-semibold'>
                                 {category}
                             </span>
                             <h2 className='text-lg font-bold my-2'>{title}</h2>
