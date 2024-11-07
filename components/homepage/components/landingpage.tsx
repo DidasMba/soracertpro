c8b0a512c5eaf906496ff77a74cdefcea7b3b884
didasmba <daniel.mbarushi@gmail.com>
2024-11-07 11:40:16 +0300

    slode

--- components/homepage/components/landingpage.tsx
+++ components/homepage/components/landingpage.tsx
@@ -58,7 +58,7 @@ const HomePage = () => {
                   </p>
                   <button className="w-[157px] py-2 rounded-4xl  bg-customHoverBlue flex flex-row items-start justify-start pt-3 px-[19px] pb-[11px] box-border whitespace-nowrap z-[1] text-center text-sm text-[#05264f]" style={{ borderRadius: '20px' }}>
                     <a className="[text-decoration:none] h-[23px] flex-1 relative font-semibold text-[inherit] inline-block z-[1] ">
-                      Learn more
+                      Learn moree
                     </a>
                   </button>
                 </div>
@@ -68,6 +68,8 @@ const HomePage = () => {
         ))}
 
       </Slider>
+
+
       <div className="mx-auto max-w-7xl px-4 rounded-lg sm:px-6 lg:px-8 top-[-110px]">
         <div className="bg-white relative -top-[90px]  rounded-2xl shadow-lg">
           <div className="flex p-4 md:p-6 justify-center items-center">
