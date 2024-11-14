89a43c39d99578a0fc016e9275afa8ea4ab28074
gracebir <grace.devolop@gmail.com>
2024-11-14 15:45:42 +0200

    typ parameter

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -4,9 +4,8 @@ import React from "react";
 import Landing from "@/components/blog/slag/landing";
 import Blogdetails from "@/components/blog/slag/details";
 import RaletedBlog from "@/components/blog/slag/raleted";
-import { Params } from "next/dist/server/request/params";
 
-export default function SingleBlogpage({ params }: { params: Params }) {
+export default function SingleBlogpage({ params }: { params: { id: string } }) {
     return (
         <>
             <Landing />
