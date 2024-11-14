19ec05c371a9b30bdfae12a37190b6d80e0410e2
didasmba <daniel.mbarushi@gmail.com>
2024-11-14 18:32:37 +0300

    solve conflit

---
+++ components/common/Heading2.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+
+const Heading: React.FC<{ text: string }> = ({ text }) => {
+    return (
+        <div>
+            <h1 className='text-2xl font-bold  text-[#05264f] mb-3 py-4'>
+                {text}
+            </h1>
+            <div className='mt-4 mb-3 h-1 w-24 bg-[#05264f]'></div>
+        </div>
+    );
+};
+
+export default Heading;
