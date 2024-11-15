3215e06198c338ad74dc3712bb408c2dc917d3e1
didasmba <daniel.mbarushi@gmail.com>
2024-11-15 20:52:16 +0300

    slove camel

--- components/career/slag/pagecareer.tsx
+++ components/career/slag/pagecareer.tsx
@@ -237,7 +237,7 @@ Bs Graduation
               <button className="py-2 rounded-4xl bg-slate-200 flex flex-row items-center justify-center pt-1  pb-[4px] box-border">
               <svg  
              
-             className=" w-5 h-5 font-mono justify-center items-center text-black"xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></button>
+             className=" w-5 h-5 font-mono justify-center items-center text-black"xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></button>
        </div>
        
 </div>
