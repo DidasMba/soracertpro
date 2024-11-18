6da26c08d1a5fdc942bf0f2ddf6c6e3d4b2968f8
didasmba <daniel.mbarushi@gmail.com>
2024-11-18 11:57:22 +0300

    separator portrait

--- components/events/slag/portrait.tsx
+++ components/events/slag/portrait.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import howitwork from '@/assets/how-we-work-mob.png';
 import Image from 'next/image'; // For Next.js Image component
+import Separator from "@/components/homepage/components/Separator";
+import HowItworkMob from "@/assets/how-we-work-mob.png";
+import HowItwork from "@/assets/how-we-work.png";
 
 const Portrait: React.FC = () => {
   return (
     <header className="container my-lg-32px">
-      <div className="row justify-content-center">
-        <div className="col-12 col-lg-10">
-          <Image
-            src={howitwork}
-            alt="Growth Marketing Bootcamp Info Session"
-            width={300}
-            height={400}
-            className="img-fluid mw-100 w-100 rounded-2 object-fit-contain max-height-250 border border-light"
-            priority // Ensures this image loads eagerly (important for above-the-fold content)
-          />
-        </div>
-      </div>
+     <Separator
+                description={`Chez Soracert, nous offrons un parcours unique pour les jeunes passionnés de technologie, alliant formation et accompagnement personnalisé. Nos programmes inspirants et immersifs permettent à chaque participant de renforcer ses compétences, de développer sa confiance et de se préparer à réussir dans un secteur en pleine croissance.`}
+                title={`Formation et développement des jeunes chez Soracert: un accompagnement vers l'excellence.`}
+                imgUrl={HowItwork}
+                imgUrlMob={HowItworkMob}
+                id='how-it-work-separator'
+            />
     </header>
   );
 };
