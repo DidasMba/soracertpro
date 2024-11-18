c98fca627651f94421c7e561134fd222b8c5a896
didasmba <daniel.mbarushi@gmail.com>
2024-11-18 09:52:03 +0300

    imgportrait

--- components/events/slag/portrait.tsx
+++ components/events/slag/portrait.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import howitwork from '@/assets/how-we-work-mob.png';
+import Image from 'next/image'; // For Next.js Image component
 
-const Portrait = () => {
+const Portrait: React.FC = () => {
   return (
-    <div>portarait</div>
-  )
-}
+    <header className="container my-lg-32px">
+      <div className="row justify-content-center">
+        <div className="col-12 col-lg-10">
+          <Image
+            src={howitwork}
+            alt="Growth Marketing Bootcamp Info Session"
+            width={300}
+            height={400}
+            className="img-fluid mw-100 w-100 rounded-2 object-fit-contain max-height-250 border border-light"
+            priority // Ensures this image loads eagerly (important for above-the-fold content)
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Portrait;
