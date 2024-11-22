9812f2a265d6738e80851f613dc242f0ae32c3ef
didasmba <daniel.mbarushi@gmail.com>
2024-11-23 00:05:39 +0300

    research ok

--- components/homepage/components/Recherche.tsx
+++ components/homepage/components/Recherche.tsx
@@ -2,40 +2,34 @@ import React from 'react';
 
 const Recherche = () => {
   return (
-    <div className="flex items-center justify-center ">
-      <form className="flex items-center max-w-sm mx-auto">
+    <div className="flex items-center justify-center">
+      <form className="flex items-center mx-auto">
         <label htmlFor="simple-search" className="sr-only">
-          Search
+          Recherche
         </label>
         <div className="relative w-full">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              className="w6 h-6 text-gray-500 dark:text-gray-400 rounded-4xl"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 18 20"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"
-              />
+
             </svg>
-          </div>
-          <input
+            </div>
+             <input
             type="text"
             id="simple-search"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search branch name..."
+            className="bg-gray-50 border border-gray-300 text-gray-900 w-[500px] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Recherche"
             required
           />
         </div>
         <button
           type="submit"
-          className="p-2.5 ms-2 text-sm font-medium text-white bg-customBlue border border-customBlue hover:bg-customBlue focus:ring-4 focus:outline-none focus:ring-customBlue dark:bg-customBlue dark:hover:bg-customBlue dark:focus:ring-customBlue rounded-xl"
+          className="p-3 ms-2 text-sm font-medium text-white bg-customBlue border border-customBlue hover:bg-customBlue focus:ring-4 focus:outline-none focus:ring-customBlue dark:bg-customBlue dark:hover:bg-customBlue dark:focus:ring-customBlue rounded-xl"
         >
           <svg
             className="w-4 h-4"
