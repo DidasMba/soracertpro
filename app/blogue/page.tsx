3ce27c1022ec30e2324530e49b0e537a4a029b8c
didasmba <daniel.mbarushi@gmail.com>
2024-11-25 18:07:14 +0300

    blogueLIst

--- app/blogue/page.tsx
+++ app/blogue/page.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 import Bl from '@/components/blogue/bl'
 import Recherche from '@/components/blogue/recherche'
 import Blpop from '@/components/blogue/blpop';
+import BlogueList from '@/components/blogue/BlogueList';
 
 export default function Blogue() {
     return (
         <main className='w-full min-h-[50svh] flex flex-col gap-8'>
             <Recherche />
+            <BlogueList />
             <Bl />
             <Blpop />
 
