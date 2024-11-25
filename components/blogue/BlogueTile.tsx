e97fc0d74d8b7d7d111427dea7f52725a6754842
didasmba <daniel.mbarushi@gmail.com>
2024-11-25 20:40:34 +0300

    title change font

--- components/blogue/BlogueTile.tsx
+++ components/blogue/BlogueTile.tsx
@@ -14,25 +14,11 @@ const BlogueTile: React.FC<{
     return (
         <article className='card bg-white border-b p-6 rounded mb-3 relative hover:bg-gray-50'>
             <div className='flex flex-col lg:flex-row lg:gap-6 gap-4'>
-                {/* Image Column */}
-                {/* <div className='w-full lg:w-1/4 mb-4 lg:mb-0'>
-                    <div className='rounded lg:h-[90px] h-[150px] lg:w-[180px] w-full'>
-                        <Image
-                            src={image}
-                            alt={title}
-                            width={270}
-                            height={135}
-                            className='w-full h-full rounded-xl object-cover'
-                            loading='lazy'
-                        />
-                    </div>
-                </div> */}
-
                 {/* Content Column */}
                 <div className='w-full lg:w-3/4 flex flex-col lg:flex-row items-center'>
                     <div className='flex flex-col w-full lg:flex-row justify-between items-center lg:items-start'>
                         <div className='w-full lg:w-2/3 mb-4 lg:mb-0'>
-                            <h2 className='text-lg font-bold my-2'>{title}</h2>
+                            <h2 className=' text-2xl md:text-3xl font-bold my-2 '>{title}</h2>
                             <div className='flex flex-col lg:flex-row items-start lg:items-center text-sm'>
                                 <small className='text-blue font-bold mb-1 lg:mb-0 '>
                                     {date}
@@ -43,6 +29,7 @@ const BlogueTile: React.FC<{
                     </div>
                 </div>
 
+                {/* Image Column */}
                 <div className='w-full lg:w-1/4 mb-4 lg:mb-0'>
                     <div className='rounded lg:h-[90px] h-[150px] lg:w-[180px] w-full'>
                         <Image
