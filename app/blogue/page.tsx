aa9cac4e7ebcc88ed3473e8cef2a54afb77cc2a1
didasmba <daniel.mbarushi@gmail.com>
2024-11-25 20:22:42 +0300

    bloguetile

--- app/blogue/page.tsx
+++ app/blogue/page.tsx
@@ -1,9 +1,7 @@
 /** @format */
 
 import React from 'react'
-import Bl from '@/components/blogue/bl'
 import Recherche from '@/components/blogue/recherche'
-import Blpop from '@/components/blogue/blpop';
 import BlogueList from '@/components/blogue/BlogueList';
 
 export default function Blogue() {
@@ -11,9 +9,6 @@ export default function Blogue() {
         <main className='w-full min-h-[50svh] flex flex-col gap-8'>
             <Recherche />
             <BlogueList />
-            <Bl />
-            <Blpop />
-
         </main>
     );
 }
--- components/blogue/BlogueTile.tsx
+++ components/blogue/BlogueTile.tsx
@@ -2,7 +2,7 @@
 
 import Image, { StaticImageData } from "next/image";
 import React from "react";
-import SignButton from "../Button";
+
 
 const BlogueTile: React.FC<{
     title: string;
@@ -12,10 +12,10 @@ const BlogueTile: React.FC<{
     id: number;
 }> = ({ title, image, location, date, id }) => {
     return (
-        <article className='card bg-white border p-6 rounded mb-3 relative hover:bg-gray-50'>
+        <article className='card bg-white border-b p-6 rounded mb-3 relative hover:bg-gray-50'>
             <div className='flex flex-col lg:flex-row lg:gap-6 gap-4'>
                 {/* Image Column */}
-                <div className='w-full lg:w-1/4 mb-4 lg:mb-0'>
+                {/* <div className='w-full lg:w-1/4 mb-4 lg:mb-0'>
                     <div className='rounded lg:h-[90px] h-[150px] lg:w-[180px] w-full'>
                         <Image
                             src={image}
@@ -26,14 +26,13 @@ const BlogueTile: React.FC<{
                             loading='lazy'
                         />
                     </div>
-                </div>
+                </div> */}
 
                 {/* Content Column */}
                 <div className='w-full lg:w-3/4 flex flex-col lg:flex-row items-center'>
                     <div className='flex flex-col w-full lg:flex-row justify-between items-center lg:items-start'>
                         <div className='w-full lg:w-2/3 mb-4 lg:mb-0'>
                             <h2 className='text-lg font-bold my-2'>{title}</h2>
-
                             <div className='flex flex-col lg:flex-row items-start lg:items-center text-sm'>
                                 <small className='text-blue font-bold mb-1 lg:mb-0 '>
                                     {date}
@@ -41,13 +40,19 @@ const BlogueTile: React.FC<{
                             </div>
                             <p className='text-xs mt-1 text-bold'>{location}</p>
                         </div>
-                        <div className='w-full lg:w-1/3 text-center'>
-                            <SignButton
-                                variant='dark'
-                                href={`/programs/${id}`}
-                                text='En savoir plus'
-                            />
-                        </div>
+                    </div>
+                </div>
+
+                <div className='w-full lg:w-1/4 mb-4 lg:mb-0'>
+                    <div className='rounded lg:h-[90px] h-[150px] lg:w-[180px] w-full'>
+                        <Image
+                            src={image}
+                            alt={title}
+                            width={270}
+                            height={135}
+                            className='w-full h-full rounded-xl object-cover'
+                            loading='lazy'
+                        />
                     </div>
                 </div>
             </div>
