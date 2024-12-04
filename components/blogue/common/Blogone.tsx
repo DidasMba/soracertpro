f4ebfdaa463e0181b3316f3ec27d72e3b00d54b5
didasmba <daniel.mbarushi@gmail.com>
2024-12-04 13:40:04 +0300

    constant

--- components/blogue/common/Blogone.tsx
+++ components/blogue/common/Blogone.tsx
@@ -1,9 +1,33 @@
+import Heading from '@/components/common/Heading'
+import Section from '@/components/common/Section'
+import Link from 'next/link'
 import React from 'react'
 
 function blogone() {
   return (
-    <div>blogone</div>
+
+    // <section id="faq-2" className="faq-2 section py-5 max-w-8xl mx-auto" >
+        
+
+    // //   </section> 
+
+    <section id="faq-2" className="faq-2 section py-5 max-w-8xl mx-auto" >
+        <div className="flex sm:flex-row flex-col items-center justify-between mt-4  p-3 m-4 max-w-7xl mx-auto">
+        <div className="mt-4 max-w-5xl  text-5xl py-2 font-extralight">
+            Python Radio 30: Catch the Fox
+        </div>
+       
+        </div>
+      </section>
+
+
+
+
   )
 }
 
 export default blogone
+
+
+
+
