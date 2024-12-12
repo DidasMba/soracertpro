e25eada9034152d3743b070e8af6c88df5c1ef0f
didasmba <daniel.mbarushi@gmail.com>
2024-12-12 12:51:22 +0300

    BlogueTile

--- components/blogue/BlogueTile.tsx
+++ components/blogue/BlogueTile.tsx
@@ -7,10 +7,10 @@ import Link from "next/link";
 const BlogueTile: React.FC<{
     title: string;
     titredescription: string;
-    image: string | StaticImageData;
+    image: string;
     date: string;
 
-    id: number;
+    id: string;
 }> = ({ title, titredescription, image, date, id }) => {
     return (
         <article className='card bg-white border-b p-6 rounded mb-3 relative hover:bg-gray-50'>
@@ -20,9 +20,9 @@ const BlogueTile: React.FC<{
                     <div className='flex flex-col w-full lg:flex-row justify-between items-center lg:items-start'>
                         <div className='w-full lg:w-2/3 mb-4 lg:mb-0'>
                             <h2 className='text-lg font-bold my-2'>{title}</h2>
-                            <p className='text-xs mt-1 text-bold'>
+                            {/* <p className='text-xs mt-1 text-bold'>
                                 {titredescription}
-                            </p>
+                            </p> */}
                             <div className='flex flex-col lg:flex-row items-start lg:items-center text-sm'>
                                 <small className='text-blue font-bold mb-1 lg:mb-0 '>
                                     {date}
