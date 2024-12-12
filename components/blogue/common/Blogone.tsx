2a3273c10a63f77d346dd5a83d4d31925bfe7af7
didasmba <daniel.mbarushi@gmail.com>
2024-12-12 14:29:22 +0300

    blog detail component

--- components/blogue/common/Blogone.tsx
+++ components/blogue/common/Blogone.tsx
@@ -1,65 +1,62 @@
+"use client";
 import React from 'react';
-import { blogueDataDetails } from '@/utils/constant';
-import Image, { StaticImageData } from "next/image";
-
-// Define the Props type
-type Props = {
-  title: string;
-  description: string;
-  image: string | StaticImageData;
-  date: string;
-  id: number;
-};
+import Image from "next/image";
+import { useQuery } from '@tanstack/react-query';
+import { getBlogBySlug } from '@/lib/api/blogue';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import style from "@/app/markdown-styles.module.css";
+import Link from 'next/link';
+import { IoMdArrowBack } from 'react-icons/io';
 
 // Functional component with proper Props typing
-const Blogone: React.FC<Props> = ({ title, description, image, date, id }) => {
+const Blogone: React.FC<{ slug: string }> = ({ slug }) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["blog", slug],
+    queryFn: () => getBlogBySlug(slug)
+  })
   return (
-    <section id="faq-2" className="faq-2 section py-5 max-w-8xl mx-auto">
-      <div className="flex sm:flex-row flex-col items-center justify-between mt-4 p-3 m-4 max-w-7xl mx-auto">
-        <div className="mt-4 max-w-5xl text-5xl py-2 font-extralight">
-          Python Radio 30: Catch the Fox
+    <div className='mb-10'>
+      {isLoading ? (
+        <div className='flex justify-center min-h-[50svh] items-center'>
+          <span className='loading loading-spinner loading-lg'></span>
         </div>
-      </div>
-
-      <div className="w-full item-center lg:w-1/4 mb-4 lg:mb-0">
-        <div className="rounded lg:h-[90px] h-[150px] lg:w-[180px] w-full">
-          {blogueDataDetails?.length > 0 ? (
-            blogueDataDetails.map((image, index) => (
-              <div key={index} className="w-full rounded-xl md:h-[40svh] h-auto">
-                <Image
-                  src={image.image}
-                  alt={image.alt || `Image ${index + 1}`}
-                  width={700}
-                  height={500}
-                  className="object-cover w-full h-full rounded-xl"
-                  priority={index === 0}
-                />
+      ) : (
+        <div className='min-h-[60svh] flex flex-col gap-6 max-w-7xl mx-auto px-4 md:px-8'>
+           <div className='flex flex-col items-start'>
+                        <Link
+                            className='bg-customBlue px-6 py-2 flex text-white gap-2 font-medium hover:bg-blue-900 rounded items-center'
+                            href={"/sora/blogue"}
+                        >
+                            <IoMdArrowBack size={20} />
+                            Retour
+                        </Link>
+                    </div>
+          <div className='flex flex-col gap-2'>
+            <p className='font-semibold text-base'>Ecrit par:</p>
+            <div className='flex items-center gap-2'>
+              <div className='h-10 w-10 rounded-full'>
+                <Image src={data?.data.author.avatar!} alt={data?.data.author.firstname!} width={40} height={40} className='w-full h-full object-cover rounded-full' />
               </div>
-            ))
-          ) : (
-            <p className="text-gray-500">No images available</p>
-          )}
-        </div>
-
-        {/* Content Column */}
-        <div className='w-full lg:w-3/4 flex flex-col lg:flex-row items-center'>
-          <div className='flex flex-col w-full lg:flex-row justify-between items-center lg:items-start'>
-            <div className='w-full lg:w-2/3 mb-4 lg:mb-0'>
-              <h2 className='text-lg font-bold my-2'>{title}</h2>
-              
-              <div className='flex flex-col lg:flex-row items-start lg:items-center text-sm'>
-                <small className='text-blue font-bold mb-1 lg:mb-0 '>
-                  {date}
-                </small>
-              </div>
-              <p className='text-xs mt-1 text-bold'>
-                {description}
-              </p>
+              <p className='text-gray-500 font-semibold'>{data?.data.author.firstname} {data?.data.author.lastname}</p>
             </div>
           </div>
+          <div className='h-[50svh] w-full bg-no-repeat bg-cover rounded-md border border-gray-400' style={{ backgroundImage: `url(${data?.data.thumbnail})` }}>
+          </div>
+          <h1 className='text-2xl md:text-3xl text-customBlue font-bold'>{data?.data.title}</h1>
+          <div className=''>
+            {data?.data.content && (
+              <ReactMarkdown
+                className={style.reactMarkDown}
+                remarkPlugins={[remarkGfm]}
+              >
+                {data.data.content}
+              </ReactMarkdown>
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      )}
+    </div>
   );
 };
 
