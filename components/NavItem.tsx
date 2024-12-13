2e27e9d520ba17e01245a9cbb71903862e24b29a
gracebir <grace.devolop@gmail.com>
2024-12-13 16:04:10 +0200

    fix the font size of the text

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { IoChevronDownOutline } from "react-icons/io5";
@@ -24,18 +25,29 @@ const NavItem: React.FC<{
                 {hasDropdown && <IoChevronDownOutline size={20} />}
             </Link>
             {hasDropdown && (
-                <div className='px-6 hidden bg-white w-[80rem] -left-56 top-6  rounded-xl z-12 absolute py-6 group-hover:flex flex-col gap-2 shadow-xl'>
-                    {subLink?.map((sublink, _) => (
-                        <Link
-                            className={`font-semibold text-sm text-customBlue hover:text-customHoverBlue whitespace-nowrap hover:underline ${
-                                isActive && `text-customHoverBlue text-sm`
-                            }`}
-                            key={`${sublink.text}-header`}
-                            href={sublink.href}
-                        >
-                            {sublink.text}
-                        </Link>
-                    ))}
+                <div className='px-6 hidden border border-gray-200 bg-white w-[80rem] -left-56 top-6  rounded-xl z-12 absolute z-10 py-6 group-hover:flex justify-between  shadow-xl'>
+                    <div className='flex flex-col gap-2'>
+                        {subLink?.map((sublink, _) => (
+                            <Link
+                                className={`font-semibold text-sm text-customBlue hover:text-customHoverBlue whitespace-nowrap hover:underline ${
+                                    isActive && `text-customHoverBlue text-sm`
+                                }`}
+                                key={`${sublink.text}-header`}
+                                href={sublink.href}
+                            >
+                                {sublink.text}
+                            </Link>
+                        ))}
+                    </div>
+                    <div className='h-full w-40'>
+                        <Image
+                            src={"/education.png"}
+                            alt='illustration'
+                            width={200}
+                            height={100}
+                            className='w-full'
+                        />
+                    </div>
                 </div>
             )}
         </div>
