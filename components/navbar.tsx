488260e67805c4891fdec0031f2e3b6c891a505c
gracebir <grace.devolop@gmail.com>
2024-12-16 17:09:37 +0200

    config the language translation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@
 import { navItems } from "@/utils/constant";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SignButton from "./Button";
 import { usePathname } from "next/navigation";
 import { LuMenu } from "react-icons/lu";
@@ -12,118 +12,199 @@ import NavItem from "./NavItem";
 import { useUser } from "@/context/UserContext";
 import { IoSettingsOutline } from "react-icons/io5";
 import { PiSignOutLight } from "react-icons/pi";
+import { GoGlobe } from "react-icons/go";
+import { LucideChevronDown } from "lucide-react";
+import { useTranslation } from "react-i18next";
+
+const languages = [
+  { code: "en", name: "English" },
+  { code: "fr", name: "Français" },
+];
 
 const Navbar: React.FC<{ isLogged: boolean }> = ({ isLogged }) => {
-    const [isScrolled, setIsScrolled] = useState(false);
-    const pathName = usePathname();
-    const [isNavMobOpen, setIsNavMobOpen] = useState(false);
-    const [isOpenDrop, setIsOpenDrop] = useState(false);
-    const user = useUser();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const pathName = usePathname();
+  const [isNavMobOpen, setIsNavMobOpen] = useState(false);
+  const [isOpenDrop, setIsOpenDrop] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setIsScrolled(window.scrollY > 40);
-        };
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [currentLang, setCurrentLang] = useState(languages[1]);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [t, i18n] = useTranslation("global");
 
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+  const user = useUser();
 
-    return (
-        <header
-            className={`fixed top-0 right-0 flex items-center left-0 w-full h-16 inset-0 ${
-                isScrolled ? "bg-gray-100" : "backdrop-blur-sm bg-white/30"
-            }  z-50 duration-300 transition-colors ease-in-out`}
-        >
-            <nav className='max-w-7xl mx-auto w-full px-4 md:px-8 flex gap-8 lg:justify-normal justify-between items-center'>
-                <Link href={"/sora/home"}>
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 40);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const locLang =
+      localStorage.getItem("langugae") || navigator.language.slice(0, 2);
+    const lang =
+      languages.find((lang) => lang.code === locLang) || languages[0];
+    i18n.changeLanguage(lang.code);
+    document.documentElement.lang = lang.code;
+    setCurrentLang(lang);
+  }, [currentLang.code, i18n]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const selectLanguage = (lang: (typeof languages)[0]) => {
+    localStorage.setItem("langugae", lang.code);
+    setCurrentLang(lang);
+    setIsDropdownOpen(false);
+    document.documentElement.lang = lang.code;
+    window.location.reload();
+  };
+
+  return (
+    <header
+      className={`fixed top-0 right-0 flex items-center left-0 w-full h-16 inset-0 ${
+        isScrolled ? "bg-gray-100" : "backdrop-blur-sm bg-white/30"
+      }  z-50 duration-300 transition-colors ease-in-out`}
+    >
+      <nav className="max-w-7xl mx-auto w-full px-4 md:px-8 flex gap-8 lg:justify-normal justify-between items-center">
+        <Link href={"/sora/home"}>
+          <Image height={55} width={100} src="/logoblack.png" alt="logo" />
+        </Link>
+        <div className="flex-1 lg:flex  sm:hidden hidden justify-between items-center">
+          <ul className="flex gap-8 flex-row">
+            {navItems.map((item) => (
+              <NavItem
+                key={item.id}
+                hasDropdown={item.hasDropdow}
+                text={item.name}
+                href={item.href}
+                subLink={item.children}
+                isActive={pathName.includes(item.pageName!)}
+              />
+            ))}
+          </ul>
+          <div className="flex gap-4">
+            {/* user logged button */}
+            {isLogged ? (
+              <div className="relative">
+                <div
+                  onClick={() => setIsOpenDrop(!isOpenDrop)}
+                  className="cursor-pointer flex items-center gap-1"
+                >
+                  <span className="text-sm font-semibold">
+                    {user.user?.firstname}
+                  </span>
+                  <div className="h-10  w-10 rounded-full border border-gray-300">
                     <Image
-                        height={55}
-                        width={100}
-                        src='/logoblack.png'
-                        alt='logo'
+                      src={user.user?.avatar!}
+                      alt="user"
+                      width={40}
+                      height={40}
+                      className="w-full h-full rounded-full"
                     />
-                </Link>
-                <div className='flex-1 lg:flex  sm:hidden hidden justify-between items-center'>
-                    <ul className='flex gap-8 flex-row'>
-                        {navItems.map((item) => (
-                            <NavItem
-                                key={item.id}
-                                hasDropdown={item.hasDropdow}
-                                text={item.name}
-                                href={item.href}
-                                subLink={item.children}
-                                isActive={pathName.includes(item.pageName!)}
-                            />
-                        ))}
-                    </ul>
-                    {isLogged ? (
-                        <div className='relative'>
-                            <div
-                                onClick={() => setIsOpenDrop(!isOpenDrop)}
-                                className='cursor-pointer flex items-center gap-1'
-                            >
-                                <span className='text-sm font-semibold'>
-                                    {user.user?.firstname}
-                                </span>
-                                <div className='h-10  w-10 rounded-full border border-gray-300'>
-                                    <Image
-                                        src={user.user?.avatar!}
-                                        alt='user'
-                                        width={40}
-                                        height={40}
-                                        className='w-full h-full rounded-full'
-                                    />
-                                </div>
-                            </div>
-                            {isOpenDrop && (
-                                <div className='absolute top-11 bg-white left-4 shadow-lg border border-gray-200 rounded-md flex flex-col gap-2 px-4 py-2'>
-                                    <button
-                                        className='flex items-center gap-1 text-sm font-medium text-customBlue hover:text-customHoverBlue'
-                                        type='button'
-                                    >
-                                        <IoSettingsOutline size={18} /> Voir
-                                        Profile
-                                    </button>
-                                    <button
-                                        className='flex items-center gap-1 text-sm font-medium text-customBlue hover:text-customHoverBlue'
-                                        type='button'
-                                    >
-                                        <PiSignOutLight size={18} />
-                                        Deconnexion
-                                    </button>
-                                </div>
-                            )}
-                        </div>
-                    ) : (
-                        <div className='flex flex-row gap-4'>
-                            <SignButton
-                                text="S'inscrire"
-                                href='/signup'
-                                variant='light'
-                            />
-                            <SignButton
-                                text='Se connecter'
-                                href='/signin'
-                                variant='dark'
-                            />
-                        </div>
-                    )}
+                  </div>
                 </div>
-                <LuMenu
-                    onClick={() => setIsNavMobOpen(true)}
-                    className='block sm:block lg:hidden'
-                    size={30}
-                />
-                <NavMob
-                    isNavMobOpen={isNavMobOpen}
-                    setIsNavMobOpen={setIsNavMobOpen}
-                />
-            </nav>
-        </header>
-    );
+                {isOpenDrop && (
+                  <div className="absolute top-11 bg-white left-4 shadow-lg border border-gray-200 rounded-md flex flex-col gap-2 px-4 py-2">
+                    <button
+                      className="flex items-center gap-1 text-sm font-medium text-customBlue hover:text-customHoverBlue"
+                      type="button"
+                    >
+                      <IoSettingsOutline size={18} /> Voir Profile
+                    </button>
+                    <button
+                      className="flex items-center gap-1 text-sm font-medium text-customBlue hover:text-customHoverBlue"
+                      type="button"
+                    >
+                      <PiSignOutLight size={18} />
+                      Deconnexion
+                    </button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div className="flex flex-row gap-4">
+                <SignButton text="S'inscrire" href="/signup" variant="light" />
+                <SignButton text="Se connecter" href="/signin" variant="dark" />
+              </div>
+            )}
+            {/* translations button */}
+            <div className="flex gap-6 items-center lg:gap-8">
+              <div
+                id="google_translate_element"
+                className={`relative`}
+                ref={dropdownRef}
+              >
+                <button
+                  className={`flex items-center gap-1 `}
+                  onClick={toggleDropdown}
+                  aria-haspopup="true"
+                  aria-expanded={isDropdownOpen}
+                >
+                  <GoGlobe />
+                  <span className={`text-sm font-bold`}>
+                    {currentLang.code.toUpperCase()}
+                  </span>
+                  <LucideChevronDown
+                    className={`transform transition-transform duration-200 ${
+                      isDropdownOpen ? "rotate-180" : ""
+                    }`}
+                  />
+                </button>
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-40 bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      {languages.map((lang) => (
+                        <button
+                          key={lang.code}
+                          className={`block w-full text-left px-4 font-semibold py-2 text-sm text-gray-700 ${
+                            currentLang.code === lang.code &&
+                            "bg-customBlue text-white"
+                          } hover:bg-customBlue hover:text-white`}
+                          onClick={() => selectLanguage(lang)}
+                        >
+                          {lang.name}
+                        </button>
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <LuMenu
+          onClick={() => setIsNavMobOpen(true)}
+          className="block sm:block lg:hidden"
+          size={30}
+        />
+        <NavMob isNavMobOpen={isNavMobOpen} setIsNavMobOpen={setIsNavMobOpen} />
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
--- lib/QueryProvider.tsx
+++ lib/QueryProvider.tsx
@@ -3,25 +3,44 @@
 "use client";
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+import global_en from "../translations/en/global.json";
+import global_fr from "../translations/fr/global.json";
+
+i18next.init({
+  interpolation: {
+    escapeValue: false,
+  },
+  lng: "fr",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    fr: {
+      global: global_fr,
+    },
+  },
+});
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-    const [queryClient] = useState(
-        () =>
-            new QueryClient({
-                defaultOptions: {
-                    queries: {
-                        // With SSR, we usually want to set some default staleTime
-                        // above 0 to avoid refetching immediately on the client
-                        staleTime: 60 * 1000,
-                    },
-                },
-            })
-    );
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    );
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <I18nextProvider i18n={i18next}>{children}</I18nextProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default QueryProvider;
