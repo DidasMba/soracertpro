cc3964cb53e07e7855fbdfaafec75b87bbbd8e1d
gracebir <grace.devolop@gmail.com>
2025-01-07 20:00:31 +0200

    get event lang

--- components/program/ProgramList.tsx
+++ components/program/ProgramList.tsx
@@ -2,12 +2,12 @@
 
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Section from "../common/Section";
 import {
-    CategoryProgramOptions,
-    dateEventFilter,
-    LocationOptions,
+  CategoryProgramOptions,
+  dateEventFilter,
+  LocationOptions,
 } from "@/utils/constant";
 import ProgramTile from "./ProgramTile";
 import Radios from "./common/Radios";
@@ -15,91 +15,102 @@ import FilterButton from "./common/FilterButton";
 import { useQuery } from "@tanstack/react-query";
 import { fetchAllProgram } from "@/lib/api/program";
 import { IoWarningOutline } from "react-icons/io5";
+import { getLanguage } from "@/lib/api/baseUrl";
 
 const ProgramList = () => {
-    const [selectedDate, setSelectedDate] = useState("all");
-    const [selectedLocation, setSelectedLocation] = useState("tous");
+  const [selectedDate, setSelectedDate] = useState("all");
+  const [selectedLocation, setSelectedLocation] = useState("tous");
 
-    const [category, setCategory] = useState("all");
+  const [category, setCategory] = useState("all");
 
-    const { data, isLoading } = useQuery({
-        queryKey: ["programs", selectedDate, category, selectedLocation],
-        queryFn: () =>
-            fetchAllProgram(selectedDate, category, selectedLocation),
-    });
+  const [lang, setLang] = useState(getLanguage());
 
-    return (
-        <Section id='program-list'>
-            <div className='grid grid-cols-1 gap-6 md:grid-cols-7 my-8'>
-                <div className='col-span-1 '>
-                    <div className='flex flex-col gap-3 py-8'>
-                        <h4 className='text-lg md:text-base font-semibold'>
-                            Filtrer
-                        </h4>
-                        <Radios
-                            name='date-filter'
-                            options={dateEventFilter}
-                            selectedDate={selectedDate}
-                            setSelectedDate={setSelectedDate}
-                        />
-                        <hr className='bg-gray-500' />
-                        <div>
-                            <h4 className='text-base font-semibold'>
-                                Trouvees
-                            </h4>
-                            <Radios
-                                name='location-filter'
-                                options={LocationOptions}
-                                selectedDate={selectedLocation}
-                                setSelectedDate={setSelectedLocation}
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col gap-4 col-span-6'>
-                    <h2 className='text-sm lg:text-base font-bold text-customBlue'>
-                        programmes et trouvez celui qui vous passionne.
-                    </h2>
-                    <FilterButton
-                        options={CategoryProgramOptions}
-                        selectedItem={category}
-                        setSelectedItem={setCategory}
-                    />
-                    {isLoading ? (
-                        <div className='flex justify-center min-h-[40svh] items-center'>
-                            <span className='loading loading-spinner loading-lg'></span>
-                        </div>
-                    ) : (
-                        <div>
-                            {data?.data.length !== 0 ? (
-                                <div className='flex flex-col gap-2'>
-                                    {data?.data.map((item) => (
-                                        <ProgramTile
-                                            key={item.id}
-                                            title={item.title}
-                                            image={item.thumbnail}
-                                            location={item.location}
-                                            date={item.date_from}
-                                            slug={item.slug}
-                                        />
-                                    ))}
-                                </div>
-                            ) : (
-                                <div className='flex  items-center w-full justify-center py-8'>
-                                    <div className='flex flex-col items-center'>
-                                        <IoWarningOutline size={40} />
-                                        <p className='text-base font-semibold text-center'>
-                                            Pas Programme trouver
-                                        </p>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    )}
-                </div>
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const updatedLang = getLanguage();
+      setLang(updatedLang);
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["programs", selectedDate, category, selectedLocation],
+    queryFn: () => fetchAllProgram(selectedDate, category, selectedLocation),
+  });
+
+  return (
+    <Section id="program-list">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-7 my-8">
+        <div className="col-span-1 ">
+          <div className="flex flex-col gap-3 py-8">
+            <h4 className="text-lg md:text-base font-semibold">Filtrer</h4>
+            <Radios
+              name="date-filter"
+              options={dateEventFilter}
+              selectedDate={selectedDate}
+              setSelectedDate={setSelectedDate}
+            />
+            <hr className="bg-gray-500" />
+            <div>
+              <h4 className="text-base font-semibold">Trouvees</h4>
+              <Radios
+                name="location-filter"
+                options={LocationOptions}
+                selectedDate={selectedLocation}
+                setSelectedDate={setSelectedLocation}
+              />
             </div>
-        </Section>
-    );
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 col-span-6">
+          <h2 className="text-sm lg:text-base font-bold text-customBlue">
+            programmes et trouvez celui qui vous passionne.
+          </h2>
+          <FilterButton
+            options={CategoryProgramOptions}
+            selectedItem={category}
+            setSelectedItem={setCategory}
+          />
+          {isLoading ? (
+            <div className="flex justify-center min-h-[40svh] items-center">
+              <span className="loading loading-spinner loading-lg"></span>
+            </div>
+          ) : (
+            <div>
+              {data?.data.length !== 0 ? (
+                <div className="flex flex-col gap-2">
+                  {data?.data.map((item) => (
+                    <ProgramTile
+                      key={item.id}
+                      title={item.title}
+                      image={item.thumbnail}
+                      location={item.location}
+                      date={item.date_from}
+                      slug={item.slug}
+                    />
+                  ))}
+                </div>
+              ) : (
+                <div className="flex  items-center w-full justify-center py-8">
+                  <div className="flex flex-col items-center">
+                    <IoWarningOutline size={40} />
+                    <p className="text-base font-semibold text-center">
+                      Pas Programme trouver
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default ProgramList;
--- type.d.ts
+++ type.d.ts
@@ -3,236 +3,242 @@
 import React from "react";
 
 export type TRadio = {
-    options: Array<{ id: string; label: string }>;
-    selectedDate: string;
-    setSelectedDate: React.Dispatch<React.SetStateAction<string>>;
-    name: string;
+  options: Array<{ id: string; label: string }>;
+  selectedDate: string;
+  setSelectedDate: React.Dispatch<React.SetStateAction<string>>;
+  name: string;
 };
 
 export type TRadioCategory = {
-    options: Array<{ id: string; label: string }>;
-    selectedItem: string;
-    setSelectedItem: React.Dispatch<React.SetStateAction<string>>;
+  options: Array<{ id: string; label: string }>;
+  selectedItem: string;
+  setSelectedItem: React.Dispatch<React.SetStateAction<string>>;
 };
 
 enum EventType {
-    ATELIER = "ATELIER",
-    HACKATHON = "HACKATHON",
-    CONFERENCE = "CONFERENCE",
+  ATELIER = "ATELIER",
+  HACKATHON = "HACKATHON",
+  CONFERENCE = "CONFERENCE",
 }
 
 type TextFieldType = {
-    label: string;
-    placeholder: string;
-    error: string;
-    touched: boolean;
-    name: string;
-    value: string | number;
-    type: React.HTMLInputTypeAttribute;
-    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    handleBlur: (e: React.FocusEvent<HTMLInputElement, Element>) => void;
+  label: string;
+  placeholder: string;
+  error: string;
+  touched: boolean;
+  name: string;
+  value: string | number;
+  type: React.HTMLInputTypeAttribute;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur: (e: React.FocusEvent<HTMLInputElement, Element>) => void;
 };
 
 type TextAreaType = {
-    label: string;
-    placeholder: string;
-    error: string;
-    touched: boolean;
-    name: string;
-    value: string | number;
-    type: React.HTMLInputTypeAttribute;
-    handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-    handleBlur: (e: React.FocusEvent<HTMLTextAreaElement, Element>) => void;
+  label: string;
+  placeholder: string;
+  error: string;
+  touched: boolean;
+  name: string;
+  value: string | number;
+  type: React.HTMLInputTypeAttribute;
+  handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  handleBlur: (e: React.FocusEvent<HTMLTextAreaElement, Element>) => void;
 };
 
 export type UserResponse = {
-    firstname: string;
-    lastname: string;
-    email: string;
-    gender: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  gender: string;
 };
 
 export type UserDataInput = {
-    firstname: string;
-    lastname: string;
-    avatar?: File | null;
-    username?: string;
-    email: string;
-    role: string;
-    password: string;
-    gender: string;
+  firstname: string;
+  lastname: string;
+  avatar?: File | null;
+  username?: string;
+  email: string;
+  role: string;
+  password: string;
+  gender: string;
 };
 
 export type PatnerDataInput = {
-    firstname: string;
-    lastname: string;
-    logo?: File | null;
-    company?: string;
-    email: string;
-    description: string;
-   
-}
+  firstname: string;
+  lastname: string;
+  logo?: File | null;
+  company?: string;
+  email: string;
+  description: string;
+};
 
 export type ApiUserResponse = {
-    status: string;
-    user: User;
+  status: string;
+  user: User;
 };
 
 export type TUser = {
-    id: number;
-    firstname: string;
-    lastname: string;
-    avatar: string;
-    email: string;
-    isVerified: boolean;
-    createdAt: string; // Can be improved for date parsing (optional)
-    updatedAt: string; // Can be improved for date parsing (optional)
+  id: number;
+  firstname: string;
+  lastname: string;
+  avatar: string;
+  email: string;
+  isVerified: boolean;
+  createdAt: string; // Can be improved for date parsing (optional)
+  updatedAt: string; // Can be improved for date parsing (optional)
 };
 
 export type Event = {
+  id: number;
+  slug: string;
+  theme: string;
+  description: string;
+  start_date: string;
+  end_date: string;
+  location: string;
+  price?: number;
+  coordinates?: Record<string, unknown>;
+  images: string[];
+  thumbnail: string;
+  isPublished?: boolean;
+  eventType: EventType;
+  createdAt: Date;
+  updatedAt: Date;
+  participants: Participant[];
+  creator: User;
+  creatorId: number;
+  moderators: Moderator[];
+  comments: Comment[];
+  EventSponsor: EventSponsor[];
+  EventTranslation: Array<{
     id: number;
-    slug: string;
+    eventId: number;
+    language: string;
     theme: string;
     description: string;
-    start_date: string;
-    end_date: string;
-    location: string;
-    price?: number;
-    coordinates?: Record<string, unknown>;
-    images: string[];
-    thumbnail: string;
-    isPublished?: boolean;
-    eventType: EventType;
-    createdAt: Date;
-    updatedAt: Date;
-    participants: Participant[];
-    creator: User;
-    creatorId: number;
-    moderators: Moderator[];
-    comments: Comment[];
-    EventSponsor: EventSponsor[];
+  }>;
 };
 
 type Pattern = {
-    avatar: string;
-    company: string;
+  avatar: string;
+  company: string;
 };
 
 type TEventParticipantInput = {
-    userId: number;
-    eventId: number;
+  userId: number;
+  eventId: number;
 };
 
 export type GetParticipantsResponse = {
-    status: string;
-    message: string;
-    data: Array<TParticipant>;
+  status: string;
+  message: string;
+  data: Array<TParticipant>;
 };
 
 export type TParticipant = {
-    id: number;
-    userId: number;
-    eventId: number;
-    isPayed: boolean;
-    user: TUser;
+  id: number;
+  userId: number;
+  eventId: number;
+  isPayed: boolean;
+  user: TUser;
 };
 export type EventSponsor = {
-    id: number;
-    eventId: number;
-    patternId: number;
-    amount?: number;
-    note?: string;
-    event: Event;
-    pattern: Pattern;
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  eventId: number;
+  patternId: number;
+  amount?: number;
+  note?: string;
+  event: Event;
+  pattern: Pattern;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export type Comment = {
-    id: number;
-    content: string;
-    createdAt: Date;
-    updatedAt: Date;
-    eventId: number;
-    event: Event;
+  id: number;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  eventId: number;
+  event: Event;
 };
 
 export type TProgramParticipantInput = {
-    userId: number;
-    programId: number;
+  userId: number;
+  programId: number;
 };
 
 export type TParticipant = {
-    id: number;
-    isPayed: boolean;
-    userId: number;
-    programId: number;
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  isPayed: boolean;
+  userId: number;
+  programId: number;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export type ApiCreateProgramResponse = {
-    status: string;
-    message: string;
-    error_message: string;
+  status: string;
+  message: string;
+  error_message: string;
 };
 
 export type Moderator = {
-    id: number;
-    firstname: string;
-    lastname: string;
-    bio: string;
-    phone: string;
-    avatar: string;
-    email: string;
-    event?: Event;
-    eventId?: number;
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  firstname: string;
+  lastname: string;
+  bio: string;
+  phone: string;
+  avatar: string;
+  email: string;
+  event?: Event;
+  eventId?: number;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export type Participant = {
-    id: number;
-    userId: number;
-    eventId: number;
-    isPayed: boolean;
-    user: User;
-    event: Event;
+  id: number;
+  userId: number;
+  eventId: number;
+  isPayed: boolean;
+  user: User;
+  event: Event;
 };
 
 export type TParticipantProgram = Array<{
-    userId: number;
-    programId: number;
-    user: TUser;
-    program: TProgram;
+  userId: number;
+  programId: number;
+  user: TUser;
+  program: TProgram;
 }>;
 
 export type TProgramParticipantResponse = {
-    status: string;
-    message: string;
-    data: TParticipantProgram;
+  status: string;
+  message: string;
+  data: TParticipantProgram;
 };
 
 export type TProgram = {
-    id: number;
-    title: string;
-    slug: string;
-    description: string;
-    location: string;
-    programCategory: string;
-    edition: string;
-    images: string[];
-    thumbnail: string;
-    price: number;
-    date_from: string;
-    date_to: string;
-    isLaunched: boolean;
-    userId: number;
-    programSponsor: any[]; // Adjust to the specific structure of sponsors if known
-    ProgramTutor: any[]; // Adjust to the specific structure of tutors if known
-    participants: any[];
-    createdAt: string;
-    updatedAt: string;
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  location: string;
+  programCategory: string;
+  edition: string;
+  images: string[];
+  thumbnail: string;
+  price: number;
+  date_from: string;
+  date_to: string;
+  isLaunched: boolean;
+  userId: number;
+  programSponsor: any[]; // Adjust to the specific structure of sponsors if known
+  ProgramTutor: any[]; // Adjust to the specific structure of tutors if known
+  participants: any[];
+  createdAt: string;
+  updatedAt: string;
 };
 // export type TPblogue = {
 //     id: number;
